784cc8af54fdc4e3d7258728f4686666b57abca1
Homework3 <laneevama@gmail.com>
2022-09-23 16:34:09 +0300

    Выполнила дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car1 = new Car("Lada Grante", "Lada", 0, "Россия", 1.7, "желтый");
+        Car car2 = new Car("Audi A8 50 L TDI quattro", "Audi", 2020, "Германия", 3.0, "черный");
+        Car car3 = new Car("BMW Z8", "BMW", 2021, "Германия", 3.0, "черный");
+        Car car4 = new Car("Kia Sportage 4", "Kia", 2018, "Южная Корея", 2.4, "красный");
+        Car car5 = new Car("Hyundai Avante", "Hyundai", 2016, "Южная Корея", 1.6, "оранжевый");
+
+        car1.informationAboutCar();
+        car2.informationAboutCar();
+        car3.informationAboutCar();
+        car4.informationAboutCar();
+        car5.informationAboutCar();
     }
 }
